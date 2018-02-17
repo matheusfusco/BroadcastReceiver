30314ae1ec42932d6c4ef92cc171af84f226f1b9
logonrm <logonrm@fiap.com.br>
2018-02-17 09:55:35 -0200

    first commit

---
+++ app/src/main/java/com/fiap/rm45401/demoorderedbroadcast/MeuReceiver.kt
@@ -0,0 +1,16 @@
+package com.fiap.rm45401.demoorderedbroadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+/**
+ * Created by logonrm on 17/02/2018.
+ */
+class MeuReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.i("DEMOORDERED", "Meu receiver")
+    }
+
+}
